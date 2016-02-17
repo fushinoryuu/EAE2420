181bb885d6189a2bee9ee8a3d4d8c4b4e539054e
Christian Munoz <fushinoryuu@gmail.com>
2016-02-17 08:15:30 -0700

    Created the asserter function that checks to makes sure an array is sorted correctly.

--- DataStructuresAndSorts/Assignment4/Caller.cs
+++ DataStructuresAndSorts/Assignment4/Caller.cs
@@ -13,31 +13,45 @@ namespace Assignment4
             Console.WriteLine("List before quick sort: [{0}]", string.Join(", ", qList));
             QuickSort qSorter = new QuickSort();
             qSorter.sort(qList, 0, qList.Length);
+            asserter(qList);
             Console.WriteLine("List after quick sort: [{0}]\n", string.Join(", ", qList));
 
             int[] sList = new[] { 2, 6, 3, 1, 9 };
             Console.WriteLine("List before selection sort: [{0}]", string.Join(", ", sList));
             SelectionSort sSorter = new SelectionSort();
             sSorter.sort(sList, 0, 0);
+            asserter(sList);
             Console.WriteLine("List after selection sort: [{0}]\n", string.Join(", ", sList));
 
             int[] iList = new[] { 2, 8, 5, 7, 3 };
             Console.WriteLine("List before insertion sort: [{0}]", string.Join(", ", iList));
             InsertionSort iSorter = new InsertionSort();
             iSorter.sort(iList, 0, 0);
+            asserter(iList);
             Console.WriteLine("List after insertion sort: [{0}]\n", string.Join(", ", iList));
 
             int[] bList = new[] { 1, 7, 3, 2, 0 };
             Console.WriteLine("List before bubble sort: [{0}]", string.Join(", ", bList));
             BubbleSort bSorter = new BubbleSort();
             bSorter.sort(bList, 0, 0);
+            asserter(bList);
             Console.WriteLine("List after bubble sort: [{0}]\n", string.Join(", ", bList));
 
             int[] mList = new[] { 6, 8, 3, 0, 1 };
             Console.WriteLine("List before merge sort: [{0}]", string.Join(", ", mList));
             MergeSort mSorter = new MergeSort();
             mSorter.sort(mList, 0, mList.Length - 1);
+            asserter(mList);
             Console.WriteLine("List after merge sort: [{0}]\n", string.Join(", ", mList));
+        }
+
+        private static void asserter(int[] numbers)
+        {
+            for(int index = 0; index < numbers.Length; index++)
+            {
+                if (index < numbers.Length - 1)
+                    Debug.Assert(numbers[index] <= numbers[index + 1], "List is not sorted in correct order.");
+            }
         }
     }
 }
