9691b4cc9685b135bf5e6e60cd08a6d13d96be03
Christian Munoz <fushinoryuu@gmail.com>
2016-02-20 18:51:22 -0700

    Finished commenting the code. Aldo made minor tweeks to the code.

--- DataStructuresAndSorts/Assignment4/MyList.cs
+++ DataStructuresAndSorts/Assignment4/MyList.cs
@@ -4,16 +4,18 @@ using System.Collections.Generic;
 
 namespace Assignment4
 {
-    // TODO Comment code
     class MyList<T> : IList<T>
     {
+        /// <summary>
+        /// Global variables for the class.
+        /// </summary>
         public T[] underlyingArray;
         private int element_count;
 
         /// <summary>
         /// Constructor. 
         /// </summary>
-        /// <param name="initial_size"></param>
+        /// <param name="initial_size">Sets the initial size of the array.</param>
         public MyList(int initial_size)
         {
             underlyingArray = new T[initial_size];
@@ -50,6 +52,10 @@ namespace Assignment4
             }
         }
 
+        /// <summary>
+        /// You can update the total count on the list.
+        /// </summary>
+        /// <param name="amount">Number to add to the count.</param>
         private void UpdateCount(int amount)
         {
             element_count += amount;
@@ -84,6 +90,7 @@ namespace Assignment4
             }
         }
 
+        // TODO Clear the items in the array
         /// <summary>
         /// Removes all items from the list.
         /// </summary>
@@ -92,6 +99,10 @@ namespace Assignment4
             element_count = 0;
         }
 
+        /// <summary>
+        /// Doubles the size of the array for the list.
+        /// </summary>
+        /// <param name="array">Array to resize.</param>
         public void Resize(T[] array)
         {
             T[] newArray = new T[array.Length * 2];
@@ -106,16 +117,20 @@ namespace Assignment4
         /// Determines whether the list contains a specific value.
         /// </summary>
         /// <param name="item">The object to locate in the list.</param>
-        /// <returns></returns>
+        /// <returns>true if item is found in the list; otherwise, false.</returns>
         public bool Contains(T item)
         {
             for (int index = 0; index < Count; index++)
                 if (EqualityComparer<T>.Default.Equals(underlyingArray[index], item))
                     return true;
-
             return false;
         }
-        
+
+        /// <summary>
+        /// Copies the elements of the list to an Array, starting at a particular Array index.
+        /// </summary>
+        /// <param name="array">The one-dimensional Array that is the destination of the elements copied from list.</param>
+        /// <param name="arrayIndex">The index in array at which copying begins.</param>
         public void CopyTo(T[] array, int arrayIndex)
         {
             if (Count == array.Length)
@@ -128,17 +143,31 @@ namespace Assignment4
         }
 
         // TODO Implement: Get Enumerator
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the list.</returns>
         public IEnumerator<T> GetEnumerator()
         {
             throw new NotImplementedException();
         }
 
         // TODO Implement: Index of
+        /// <summary>
+        /// Determines the index of a specific item in the list.
+        /// </summary>
+        /// <param name="item">The object to locate in the list.</param>
+        /// <returns>The index of item if found in the list; otherwise, -1.</returns>
         public int IndexOf(T item)
         {
             throw new NotImplementedException();
         }
-        
+
+        /// <summary>
+        /// Inserts an item to the list at the specified index.
+        /// </summary>
+        /// <param name="index">The index at which item should be inserted.</param>
+        /// <param name="item">The object to insert into the list.</param>
         public void Insert(int index, T item)
         {
             if (Count == underlyingArray.Length)
@@ -153,6 +182,12 @@ namespace Assignment4
             }
         }
 
+        /// <summary>
+        /// Removes the first occurrence of a specific object from the list.
+        /// </summary>
+        /// <param name="item">The object to remove from the list.</param>
+        /// <returns>true if item was successfully removed from the list; otherwise, false. 
+        /// This method also returns false if item is not found in the list</returns>
         public bool Remove(T item)
         {
             for (int index = 0; index < Count; index++)
@@ -164,6 +199,10 @@ namespace Assignment4
             return false;
         }
 
+        /// <summary>
+        /// Removes the item at the specified index.
+        /// </summary>
+        /// <param name="index">The index of the item to remove.</param>
         public void RemoveAt(int index)
         {
             while (index < Count - 1)
@@ -176,6 +215,10 @@ namespace Assignment4
         }
 
         // TODO Implement: Get IEnumerable
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection.
+        /// </summary>
+        /// <returns>An IEnumerator object that can be used to iterate through the list.</returns>
         IEnumerator IEnumerable.GetEnumerator()
         {
             throw new NotImplementedException();
