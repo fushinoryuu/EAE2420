915d4a2e1616ff451b7a79b315312f922ac639e7
Christian Munoz <fushinoryuu@gmail.com>
2016-03-25 17:05:35 -0600

    The next assignment I will be working on for class in Heaps. I am starting the basic structure of my heap class.

---
+++ DataStructuresAndSorts/Assignment6/Heap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment6
+{
+    class Heap<T> where T : IComparable<T>
+    {
+        public HeapNode<T> root;
+        IComparer<T> compare;
+
+        public Heap()
+        {
+
+        }
+
+        public void Add(T newItem, HeapNode<T> currentNode)
+        {
+
+        }
+    }
+}
---
+++ DataStructuresAndSorts/Assignment6/HeapNode.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment6
+{
+    class HeapNode<T>
+    {
+        public T Value { get; set; }
+        HeapNode<T> Left { get; set; }
+        HeapNode<T> Right { get; set; }
+    }
+}
