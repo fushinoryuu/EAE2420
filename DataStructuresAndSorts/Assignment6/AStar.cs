31de295dfe4f6362617512492e4dd97bbf0046cd
Christian Munoz <u0556573@utah.edu>
2016-04-04 21:19:18 -0600

    Dummy commit.

--- DataStructuresAndSorts/Assignment6/AStar.cs
+++ DataStructuresAndSorts/Assignment6/AStar.cs
@@ -5,161 +5,158 @@ namespace Assignment6
 {
     class AStar
     {
-        private GraphNode A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, goal, start;
-        private List<GraphNode> openList, closeList;
+        private GraphNode NodeA, NodeB, NodeC, NodeD, NodeE, NodeF, NodeG, NodeH, NodeI, NodeJ, NodeK, NodeL, NodeM, NodeN, NodeO, NodeP, Goal, Start;
+        private List<GraphNode> OpenList, CloseList;
 
-        public AStar(string inputStart, string inputEnd)
+        public AStar(string input_start, string input_end)
         {
-            openList = new List<GraphNode>();
-            closeList = new List<GraphNode>();
-            List<GraphNode> nodeList = new List<GraphNode>();
+            OpenList = new List<GraphNode>();
+            CloseList = new List<GraphNode>();
+            List<GraphNode> node_list = new List<GraphNode>();
 
-            BuildNodes(nodeList);
+            BuildNodes(node_list);
             WireConnections();
-            SetGoals(nodeList, inputStart, inputEnd);
-            Initialize();
+            SetGoals(node_list, input_start, input_end);
+            ProcessNode(Start);
         }
 
-        private void BuildNodes(List<GraphNode> nodeList)
+        private void BuildNodes(List<GraphNode> node_list)
         {
-            A = new GraphNode() { Name = "A", X = -19, Y = 11, Connections = new Connection[2] };
-            B = new GraphNode() { Name = "B", X = -13, Y = 13, Connections = new Connection[2] };
-            C = new GraphNode() { Name = "C", X = 4, Y = 14, Connections = new Connection[3] };
-            D = new GraphNode() { Name = "D", X = -4, Y = 12, Connections = new Connection[3] };
-            E = new GraphNode() { Name = "E", X = -8, Y = 3, Connections = new Connection[7] };
-            F = new GraphNode() { Name = "F", X = -18, Y = 1, Connections = new Connection[2] };
-            G = new GraphNode() { Name = "G", X = -12, Y = -8, Connections = new Connection[3] };
-            H = new GraphNode() { Name = "H", X = 12, Y = -9, Connections = new Connection[3] };
-            I = new GraphNode() { Name = "I", X = -18, Y = -11, Connections = new Connection[2] };
-            J = new GraphNode() { Name = "J", X = -4, Y = -11, Connections = new Connection[5] };
-            K = new GraphNode() { Name = "K", X = -12, Y = -14, Connections = new Connection[3] };
-            L = new GraphNode() { Name = "L", X = 2, Y = -18, Connections = new Connection[3] };
-            M = new GraphNode() { Name = "M", X = 18, Y = -13, Connections = new Connection[3] };
-            N = new GraphNode() { Name = "N", X = 4, Y = -9, Connections = new Connection[3] };
-            O = new GraphNode() { Name = "O", X = 22, Y = 11, Connections = new Connection[2] };
-            P = new GraphNode() { Name = "P", X = 18, Y = 3, Connections = new Connection[4] };
+            NodeA = new GraphNode() { Name = "A", X = -19, Y = 11, Connections = new Connection[2] };
+            NodeB = new GraphNode() { Name = "B", X = -13, Y = 13, Connections = new Connection[2] };
+            NodeC = new GraphNode() { Name = "C", X = 4, Y = 14, Connections = new Connection[3] };
+            NodeD = new GraphNode() { Name = "D", X = -4, Y = 12, Connections = new Connection[3] };
+            NodeE = new GraphNode() { Name = "E", X = -8, Y = 3, Connections = new Connection[7] };
+            NodeF = new GraphNode() { Name = "F", X = -18, Y = 1, Connections = new Connection[2] };
+            NodeG = new GraphNode() { Name = "G", X = -12, Y = -8, Connections = new Connection[3] };
+            NodeH = new GraphNode() { Name = "H", X = 12, Y = -9, Connections = new Connection[3] };
+            NodeI = new GraphNode() { Name = "I", X = -18, Y = -11, Connections = new Connection[2] };
+            NodeJ = new GraphNode() { Name = "J", X = -4, Y = -11, Connections = new Connection[5] };
+            NodeK = new GraphNode() { Name = "K", X = -12, Y = -14, Connections = new Connection[3] };
+            NodeL = new GraphNode() { Name = "L", X = 2, Y = -18, Connections = new Connection[3] };
+            NodeM = new GraphNode() { Name = "M", X = 18, Y = -13, Connections = new Connection[3] };
+            NodeN = new GraphNode() { Name = "N", X = 4, Y = -9, Connections = new Connection[3] };
+            NodeO = new GraphNode() { Name = "O", X = 22, Y = 11, Connections = new Connection[2] };
+            NodeP = new GraphNode() { Name = "P", X = 18, Y = 3, Connections = new Connection[4] };
 
-            nodeList.Add(A);
-            nodeList.Add(B);
-            nodeList.Add(C);
-            nodeList.Add(D);
-            nodeList.Add(E);
-            nodeList.Add(F);
-            nodeList.Add(G);
-            nodeList.Add(H);
-            nodeList.Add(I);
-            nodeList.Add(J);
-            nodeList.Add(K);
-            nodeList.Add(L);
-            nodeList.Add(M);
-            nodeList.Add(N);
-            nodeList.Add(O);
-            nodeList.Add(P);
+            node_list.Add(NodeA);
+            node_list.Add(NodeB);
+            node_list.Add(NodeC);
+            node_list.Add(NodeD);
+            node_list.Add(NodeE);
+            node_list.Add(NodeF);
+            node_list.Add(NodeG);
+            node_list.Add(NodeH);
+            node_list.Add(NodeI);
+            node_list.Add(NodeJ);
+            node_list.Add(NodeK);
+            node_list.Add(NodeL);
+            node_list.Add(NodeM);
+            node_list.Add(NodeN);
+            node_list.Add(NodeO);
+            node_list.Add(NodeP);
         }
 
         private void WireConnections()
         {
-            A.Connections[0] = new Connection() { Target = B };
-            A.Connections[1] = new Connection() { Target = E };
+            NodeA.Connections[0] = new Connection() { Target = NodeB };
+            NodeA.Connections[1] = new Connection() { Target = NodeE };
 
-            B.Connections[0] = new Connection() { Target = A };
-            B.Connections[1] = new Connection() { Target = D };
+            NodeB.Connections[0] = new Connection() { Target = NodeA };
+            NodeB.Connections[1] = new Connection() { Target = NodeD };
 
-            C.Connections[0] = new Connection() { Target = D };
-            C.Connections[1] = new Connection() { Target = E };
-            C.Connections[2] = new Connection() { Target = P };
+            NodeC.Connections[0] = new Connection() { Target = NodeD };
+            NodeC.Connections[1] = new Connection() { Target = NodeE };
+            NodeC.Connections[2] = new Connection() { Target = NodeP };
 
-            D.Connections[0] = new Connection() { Target = B };
-            D.Connections[1] = new Connection() { Target = C };
-            D.Connections[2] = new Connection() { Target = E };
+            NodeD.Connections[0] = new Connection() { Target = NodeB };
+            NodeD.Connections[1] = new Connection() { Target = NodeC };
+            NodeD.Connections[2] = new Connection() { Target = NodeE };
 
-            E.Connections[0] = new Connection() { Target = A };
-            E.Connections[1] = new Connection() { Target = C };
-            E.Connections[2] = new Connection() { Target = D };
-            E.Connections[3] = new Connection() { Target = G };
-            E.Connections[4] = new Connection() { Target = H };
-            E.Connections[5] = new Connection() { Target = J };
-            E.Connections[6] = new Connection() { Target = N };
+            NodeE.Connections[0] = new Connection() { Target = NodeA };
+            NodeE.Connections[1] = new Connection() { Target = NodeC };
+            NodeE.Connections[2] = new Connection() { Target = NodeD };
+            NodeE.Connections[3] = new Connection() { Target = NodeG };
+            NodeE.Connections[4] = new Connection() { Target = NodeH };
+            NodeE.Connections[5] = new Connection() { Target = NodeJ };
+            NodeE.Connections[6] = new Connection() { Target = NodeN };
 
-            F.Connections[0] = new Connection() { Target = G };
-            F.Connections[1] = new Connection() { Target = I };
+            NodeF.Connections[0] = new Connection() { Target = NodeG };
+            NodeF.Connections[1] = new Connection() { Target = NodeI };
 
-            G.Connections[0] = new Connection() { Target = F };
-            G.Connections[1] = new Connection() { Target = E };
-            G.Connections[2] = new Connection() { Target = J };
+            NodeG.Connections[0] = new Connection() { Target = NodeF };
+            NodeG.Connections[1] = new Connection() { Target = NodeE };
+            NodeG.Connections[2] = new Connection() { Target = NodeJ };
 
-            H.Connections[0] = new Connection() { Target = N };
-            H.Connections[1] = new Connection() { Target = E };
-            H.Connections[2] = new Connection() { Target = P };
+            NodeH.Connections[0] = new Connection() { Target = NodeN };
+            NodeH.Connections[1] = new Connection() { Target = NodeE };
+            NodeH.Connections[2] = new Connection() { Target = NodeP };
 
-            I.Connections[0] = new Connection() { Target = F };
-            I.Connections[1] = new Connection() { Target = K };
+            NodeI.Connections[0] = new Connection() { Target = NodeF };
+            NodeI.Connections[1] = new Connection() { Target = NodeK };
 
-            J.Connections[0] = new Connection() { Target = K };
-            J.Connections[1] = new Connection() { Target = G };
-            J.Connections[2] = new Connection() { Target = E };
-            J.Connections[3] = new Connection() { Target = N };
-            J.Connections[4] = new Connection() { Target = L };
+            NodeJ.Connections[0] = new Connection() { Target = NodeK };
+            NodeJ.Connections[1] = new Connection() { Target = NodeG };
+            NodeJ.Connections[2] = new Connection() { Target = NodeE };
+            NodeJ.Connections[3] = new Connection() { Target = NodeN };
+            NodeJ.Connections[4] = new Connection() { Target = NodeL };
 
-            K.Connections[0] = new Connection() { Target = I };
-            K.Connections[1] = new Connection() { Target = J };
-            K.Connections[2] = new Connection() { Target = L };
+            NodeK.Connections[0] = new Connection() { Target = NodeI };
+            NodeK.Connections[1] = new Connection() { Target = NodeJ };
+            NodeK.Connections[2] = new Connection() { Target = NodeL };
 
-            L.Connections[0] = new Connection() { Target = K };
-            L.Connections[1] = new Connection() { Target = J };
-            L.Connections[2] = new Connection() { Target = M };
+            NodeL.Connections[0] = new Connection() { Target = NodeK };
+            NodeL.Connections[1] = new Connection() { Target = NodeJ };
+            NodeL.Connections[2] = new Connection() { Target = NodeM };
 
-            M.Connections[0] = new Connection() { Target = L };
-            M.Connections[1] = new Connection() { Target = P };
-            M.Connections[2] = new Connection() { Target = O };
+            NodeM.Connections[0] = new Connection() { Target = NodeL };
+            NodeM.Connections[1] = new Connection() { Target = NodeP };
+            NodeM.Connections[2] = new Connection() { Target = NodeO };
 
-            N.Connections[0] = new Connection() { Target = J };
-            N.Connections[1] = new Connection() { Target = E };
-            N.Connections[2] = new Connection() { Target = H };
+            NodeN.Connections[0] = new Connection() { Target = NodeJ };
+            NodeN.Connections[1] = new Connection() { Target = NodeE };
+            NodeN.Connections[2] = new Connection() { Target = NodeH };
 
-            O.Connections[0] = new Connection() { Target = P };
-            O.Connections[1] = new Connection() { Target = M };
+            NodeO.Connections[0] = new Connection() { Target = NodeP };
+            NodeO.Connections[1] = new Connection() { Target = NodeM };
 
-            P.Connections[0] = new Connection() { Target = H };
-            P.Connections[1] = new Connection() { Target = C };
-            P.Connections[2] = new Connection() { Target = O };
-            P.Connections[3] = new Connection() { Target = M };
+            NodeP.Connections[0] = new Connection() { Target = NodeH };
+            NodeP.Connections[1] = new Connection() { Target = NodeC };
+            NodeP.Connections[2] = new Connection() { Target = NodeO };
+            NodeP.Connections[3] = new Connection() { Target = NodeM };
         }
 
         private void SetGoals(List<GraphNode> nodeList, string inputStart, string inputEnd)
         {
             foreach (GraphNode currentNode in nodeList)
-            {
                 if (currentNode.Name == inputStart)
-                {
-                    start = currentNode;
-                }
+                    Start = currentNode;
                 else if (currentNode.Name == inputEnd)
-                {
-                    goal = currentNode;
-                }
+                    Goal = currentNode;
+        }
+
+        private void ProcessNode(GraphNode current_node)
+        {
+            for (int index = 0; index < current_node.Connections.Length; index++)
+            {
+                current_node.Connections[index].CostSoFar = CalcDistance(Start, current_node);
+                current_node.Heuristic = CalcDistance(current_node, Goal);
+                current_node.Connections[index].TotalEstimatedCost =
+                    current_node.Connections[index].CostSoFar + current_node.Heuristic;
+                if (current_node.Name == Start.Name)
+                    current_node.Connections[index].From = null;
+                else
+                    current_node.Connections[index].From = current_node; // FIX
             }
+            OpenList.Add(current_node);
         }
 
-        private void Initialize()
+        private void ProcessOutBound()
         {
-            start.Connections[0].CostSoFar = 0;
-            start.Heuristic = CalcDistance(start, goal);
-            start.Connections[0].TotalEstimatedCost = start.Connections[0].CostSoFar + start.Heuristic;
-            start.Connections[0].From = null;
-            openList.Add(start);
-            PickNextNode();
+
         }
 
-        /// <summary>
-        /// Find the node with the lowest total extimated cost. 
-        /// </summary>
         private void PickNextNode()
-        {
-            
-        }
-
-        private void ProcessNode(GraphNode currentNode)
         {
 
         }
@@ -167,6 +164,11 @@ namespace Assignment6
         private double CalcDistance(GraphNode start, GraphNode target)
         {
             return Math.Sqrt(Math.Pow(target.X - start.X, 2) + Math.Pow(target.Y - start.Y, 2));
+        }
+
+        private void ReconstructPath(GraphNode came_from, GraphNode current)
+        {
+
         }
     }
 }
