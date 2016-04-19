97c5fdad0aff37e9211125323d02d0f654750396
Christian Munoz <u0556573@utah.edu>
2016-04-19 10:06:20 -0600

    Fixed the bug that was causing me to go out of bounds.

--- DataStructuresAndSorts/Assignment8/Grid.cs
+++ DataStructuresAndSorts/Assignment8/Grid.cs
@@ -51,7 +51,5 @@ namespace Assignment8
             }
             Console.WriteLine();
         }
-
-
     }
 }
--- DataStructuresAndSorts/Assignment8/KeepInBounds.cs
+++ DataStructuresAndSorts/Assignment8/KeepInBounds.cs
@@ -11,12 +11,12 @@
 
         public override void Update()
         {
-            if (Container.Position.X > bounds)
-                Container.Position.X = bounds;
+            if (Container.Position.X > bounds - 1)
+                Container.Position.X = bounds - 1;
             if (Container.Position.X < 0)
                 Container.Position.X = 0;
-            if (Container.Position.Y > bounds)
-                Container.Position.Y = bounds;
+            if (Container.Position.Y > bounds - 1)
+                Container.Position.Y = bounds - 1;
             if (Container.Position.Y < 0)
                 Container.Position.Y = 0;
         }
