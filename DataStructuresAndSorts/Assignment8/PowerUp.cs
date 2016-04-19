1a1ab8dba63e1a4f7b275792664af600e1d46d65
Christian Munoz <u0556573@utah.edu>
2016-04-19 14:39:14 -0600

    Created PowerUp class.

---
+++ DataStructuresAndSorts/Assignment8/PowerUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment8
+{
+    class PowerUp
+    {
+        public string Name { get; set; }
+        public Point Position { get; set; }
+        public Entity Container { get; set; }
+        private int Turns { get; set; }
+
+        public virtual void Update() { }
+    }
+}
