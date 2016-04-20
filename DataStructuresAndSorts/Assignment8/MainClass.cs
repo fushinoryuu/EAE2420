0c7251307fc9aa82888ad5fcaeaf28351df836ec
Christian Munoz <fushinoryuu@gmail.com>
2016-04-20 09:39:08 -0600

    Finished implementing the Fast monster. I have another randomizer that will spawn either a slow or fast monster.

--- DataStructuresAndSorts/Assignment8/MainClass.cs
+++ DataStructuresAndSorts/Assignment8/MainClass.cs
@@ -15,8 +15,9 @@ namespace Assignment8
         {
             Console.WriteLine("Use the WASD keys to move your character (P) around the grid.\n");
             Console.WriteLine("Make sure that you don't collide with Monsters or you will die and the game ends.\n");
-            Console.WriteLine("Monsters come in two types, Slow monsters (S) and Fast monsters (F) and can move randomly in any \ndirection.\n");
+            Console.WriteLine("Monsters come in two types, Slow monsters (S) and Fast monsters (F) and can move \nrandomly in any direction.\n");
             Console.WriteLine("Slow monsters move 1 space at a time, while Fast monsters move 2 spaces at a time.\n");
+            Console.WriteLine("Monsters can only wrap around the grid, but the player can not.\n");
             Console.WriteLine("Collect Power Ups to stay alive longer by walking over them:\n");
             Console.WriteLine("- T will Teleport you randomly to any place in the grid.\n");
             Console.WriteLine("- I will make you Invulnerable until your next collision.\n");
@@ -64,11 +65,34 @@ namespace Assignment8
 
         private static void AddMonster(List<Entity> list, Random randomizer, Entity player, int board_size)
         {
+            if (RandomBool())
+                SlowMonster(list, randomizer, player, board_size);
+            else
+                FastMonster(list, randomizer, player, board_size);
+        }
+
+        private static bool RandomBool()
+        {
+            return new Random().Next() % 2 == 0;
+        }
+
+        private static void SlowMonster(List<Entity> list, Random randomizer, Entity player, int board_size)
+        {
             Entity new_monster = new Entity(randomizer.Next(0, board_size), randomizer.Next(0, board_size));
             new_monster.Name = "S";
             new_monster.AddComponent(new WrapAround(board_size));
             new_monster.AddComponent(new KillOnContact(player));
             new_monster.AddComponent(new AiMovementSlow());
+            list.Add(new_monster);
+        }
+
+        private static void FastMonster(List<Entity> list, Random randomizer, Entity player, int board_size)
+        {
+            Entity new_monster = new Entity(randomizer.Next(0, board_size), randomizer.Next(0, board_size));
+            new_monster.Name = "F";
+            new_monster.AddComponent(new WrapAround(board_size));
+            new_monster.AddComponent(new KillOnContact(player));
+            new_monster.AddComponent(new AiMovementFast());
             list.Add(new_monster);
         }
 
