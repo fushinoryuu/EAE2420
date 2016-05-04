6dfd0e5be48f61631a1d63b305df69750453e349
Christian Munoz <fushinoryuu@gmail.com>
2016-05-04 14:32:31 -0600

    Since the Min and Max Heap classes share so much code, created an abstract class called Heap that the other two classes will inherit from.

---
+++ DataStructuresAndSorts/Assignment9/MaxHeapTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assignment9
+{
+    class MaxHeapTest<T> : Heap<T> where T : IComparable
+    {
+        public MaxHeapTest()
+        {
+            UnderlyingArray = new T[5];
+        }
+        
+        public MaxHeapTest(int starting_size)
+        {
+            UnderlyingArray = new T[starting_size];
+        }
+
+        public override void Add(T new_value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override T PopTop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Sort()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
