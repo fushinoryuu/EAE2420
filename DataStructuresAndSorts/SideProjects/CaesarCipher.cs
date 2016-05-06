88f2c016de8e8fa68c896b2b7460506e897c21d9
Christian Munoz <fushinoryuu@gmail.com>
2016-05-06 17:02:01 -0600

    Going to try and make a Ceasar Cipher.
    Basic code

---
+++ DataStructuresAndSorts/SideProjects/CaesarCipher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideProjects
+{
+    class CaesarCipher
+    {
+        private string Sentence;
+        private int Shift;
+        private bool IsEncrypted = false;
+
+        public CaesarCipher(string input, int shifting)
+        {
+            Sentence = input;
+            Shift = shifting;
+        }
+
+        public string Encrypt()
+        {
+            if (IsEncrypted)
+                return Sentence;
+            throw new NotImplementedException();
+        }
+
+        public string Decrypt()
+        {
+            if (!IsEncrypted)
+                return Sentence;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataStructuresAndSorts/SideProjects/Caller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideProjects
+{
+    class Caller
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter a string you wish to encrypt: \n");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("Please enter a value to shift the letters: \n");
+            int shift = int.Parse(Console.ReadLine());
+
+            CaesarCipher cipher = new CaesarCipher(input, shift);
+
+            Console.WriteLine(cipher.Encrypt());
+
+            Console.WriteLine(cipher.Decrypt());
+        }
+    }
+}
