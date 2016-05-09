7d75775abe728783bd4caac92af87994bd0c666c
Christian Munoz <u0556573@utah.edu>
2016-05-08 19:16:04 -0600

    Caesar Cipher is complete.

--- DataStructuresAndSorts/SideProjects/CaesarCipher.cs
+++ DataStructuresAndSorts/SideProjects/CaesarCipher.cs
@@ -1,35 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SideProjects
+﻿namespace SideProjects
 {
     class CaesarCipher
     {
         private string Sentence;
-        private int Shift;
+        private int EncryptShift;
+        private int DecryptShift;
         private bool IsEncrypted = false;
 
         public CaesarCipher(string input, int shifting)
         {
-            Sentence = input;
-            Shift = shifting;
+            Sentence = input.ToLower();
+            EncryptShift = shifting;
+            DecryptShift = -shifting;
         }
 
         public string Encrypt()
         {
             if (IsEncrypted)
                 return Sentence;
-            throw new NotImplementedException();
+
+            char[] char_array = Sentence.ToCharArray();
+
+            Shuffle(char_array, EncryptShift);
+
+            Recunstruct(char_array);
+
+            return Sentence;
         }
 
         public string Decrypt()
         {
             if (!IsEncrypted)
                 return Sentence;
-            throw new NotImplementedException();
+
+            char[] char_array = Sentence.ToCharArray();
+
+            Shuffle(char_array, DecryptShift);
+
+            Recunstruct(char_array);
+
+            return Sentence;
+        }
+
+        private void Shuffle(char[] char_array, int shift)
+        {
+            for (int index = 0; index < char_array.Length; index++)
+            {
+                char letter = char_array[index];
+
+                if (letter >= 'a' && letter <= 'z')
+                {
+                    letter = (char)(letter + shift);
+
+                    if (letter > 'z')
+                        letter = (char)(letter - 26);
+
+                    else if (letter < 'a')
+                        letter = (char)(letter + 26);
+                }
+
+                char_array[index] = letter;
+            }
+        }
+
+        private void Recunstruct(char[] char_array)
+        {
+            Sentence = null;
+
+            foreach (char letter in char_array)
+                Sentence += letter;
+
+            IsEncrypted = true;
         }
     }
 }
